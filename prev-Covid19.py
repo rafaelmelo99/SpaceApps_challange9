4f1c23334a68ed34dfe31437a0fd38db81b5fd09
Rafael de Melo <49524408+rafaelmelo99@users.noreply.github.com>
2020-05-31 21:17:38 -0300

    Add files via upload

---
+++ prev-Covid19.py
@@ -0,0 +1,58 @@
+import numpy as np 
+import pandas as pd 
+import keras
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from keras.utils import plot_model 
+from keras.optimizers import SGD
+
+#get data
+
+data = pd.read_csv("covid_final.csv")
+
+data['indice de Gini'] = data['indice de Gini'].str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float)
+data['IDHM 2010'] = data['IDHM 2010'].str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float)
+data['IDHM Renda 2010'] = data['IDHM Renda 2010'].str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float)
+data['IDHM Longevidade 2010'] = data['IDHM Longevidade 2010'].str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float)
+data['IDHM Educacao 2010'] = data['IDHM Educacao 2010'].str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float)
+
+#drop some cities
+#data.drop(data[data.confirmed > 5000].index, inplace=True)
+
+X = data.iloc[:, 3:data.shape[1]].values
+y = data.iloc[:,1:2].values
+
+
+
+#normalization
+
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
+
+X = scaler_x.fit_transform(X)
+y = scaler_y.fit_transform(y)
+
+
+
+#split data
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
+
+#Linear Regression
+
+from sklearn.linear_model import LinearRegression
+reglin = LinearRegression()
+reglin.fit(X_train, y_train)
+y_prev = reglin.predict(X_test)
+
+
+result = scaler_y.inverse_transform(y_prev)
+y_test = scaler_y.inverse_transform(y_test)
+
+#result
+
+print(metrics.mean_absolute_error(y_test, y_prev))
